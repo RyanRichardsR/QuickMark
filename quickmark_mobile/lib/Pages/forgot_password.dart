9f5a37c9c4a936e13648055353d72089d2320068
RyanRichardsR <ryanrrr2022@gmail.com>
2024-11-11 19:46:24 -0500

    forgot password working on mobile

--- quickmark_mobile/lib/Pages/forgot_password.dart
+++ quickmark_mobile/lib/Pages/forgot_password.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quickmark_mobile/Pages/login_page.dart';
 import 'package:quickmark_mobile/components/input_field.dart';
+import 'package:quickmark_mobile/server_calls.dart';
 
 //Color Pallete Constants
 const white = Color(0xFFF7FCFF) ;
@@ -17,6 +19,37 @@ class ForgotPassword extends StatefulWidget {
 
 class _ForgotPasswordState extends State<ForgotPassword> {
   final emailController = TextEditingController();
+  String errorMessage = '';
+
+  void resetPassword(String email, context) async {
+     //Json data
+    final body = {
+      'email': email,
+    };
+    
+    //Make API call
+    try {
+      var response = await ServerCalls().post('/forgotPassword', body);
+      if(response['error'] != '') {
+        setState((){ //Creates the error message
+          errorMessage = response['error'];
+        });
+      } else { 
+        debugPrint('Login successful: ${response['user']}');
+        setState((){ //Reset Error message
+          errorMessage = response['error'];
+        });
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const LoginPage(success: false),
+          ),
+        );
+      }
+    } catch (err) {
+      debugPrint('Error: $err');
+    }
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -27,7 +60,20 @@ class _ForgotPasswordState extends State<ForgotPassword> {
       body: SafeArea(
         child: Center(
           child: Column(
+            
             children: [
+              AppBar(
+                title: const Text(
+                  'Forgot Password',
+                  style: TextStyle(fontSize: 18),                
+                ),
+                leading: IconButton(
+                  icon: const Icon(Icons.arrow_back_ios, color: Colors.grey),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+                 backgroundColor: Colors.blue.withOpacity(0),
+                elevation: 0.0, //No shadow
+              ),
               //Spacer for spacing
               const SizedBox(height: 50),
           
@@ -46,7 +92,7 @@ class _ForgotPasswordState extends State<ForgotPassword> {
               const SizedBox(height: 50),
 
               const Text(
-                'Reset Password',
+                'Forgot your password?',
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
 
@@ -62,13 +108,14 @@ class _ForgotPasswordState extends State<ForgotPassword> {
               const SizedBox(height: 10),
 
               const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 2),
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      'Email Address:',
-                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: navy),
+                      'Enter the email you used to register with us\n and we will send you a link for a password reset!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: navy,),
                     ),
                   ],
                 ),
@@ -78,6 +125,34 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                 input: emailController,
                 hintText: 'Email',
                 obscureText: false, 
+              ),
+
+              //Spacer for spacing
+              const SizedBox(height: 30),
+
+              //Submit button
+              SizedBox(
+                width: 250,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: () {
+                    resetPassword(emailController.text, context);                  
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: blue,                   
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),                                
+                  ),
+                  child: const Text(
+                    'Reset Password',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
               ),
 
           ]),
